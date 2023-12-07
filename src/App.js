25907d94da26770cc028a4b5d93f5c9db4b5c147
Sazith <sazith.simecsoftdev@gmail.com>
2023-12-07 16:01:10 +0600

    country api added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import Country from './components/Country';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [country,setCountry] = useState([]);
+  useEffect(()=>{
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => setCountry(data))
+  },[])
+  const [cart,setCart] = useState([]);
+  const handleAddCountry = (country) =>{
+    const newCart = [...cart,cart];
+    setCart(newCart)
+
+  }
   return (
     <div className="App">
       <header className="App-header">
         <div>
-          <h4 style={{color:'red'}}>Country</h4>
+          <h2 style={{color:'red'}}>Country Api</h2>
+        </div>
+        <div>
+          <h3>Total Countries showing : {country.length}</h3>
+          <h2>Add Country :{cart.length} </h2>
+          {
+            country.map((country,i) => <Country country={country} handleAddCountry={handleAddCountry} key={i}></Country> )
+          }
         </div>
       </header>
     </div>
---
+++ src/components/Country.jsx
@@ -0,0 +1,40 @@
+import React from "react";
+
+const Country = (props) => {
+  const { name, population, region, flag, flags } = props.country;
+  const handleAddCountry = props.handleAddCountry;
+  return (
+    <>
+      <div
+        style={{
+          border: "3px dashed #109161",
+          marginBottom: "1rem",
+          padding: "1rem",
+        }}
+      >
+        <h4 style={{ margin: "0", padding: "20px 0px" }}>
+          This is {name.common}
+        </h4>
+        <div>
+          <img
+            src={flags.png}
+            alt=""
+            style={{ height: "100px", width: "150px" }}
+          />
+        </div>
+        <p style={{ margin: "0", padding: "15px 0px" }}>
+          population : {population}
+        </p>
+        <p style={{ margin: "0", padding: "15px 0px" }}>
+          <small>Region: {region} </small>
+        </p>
+        <button onClick={() => handleAddCountry(props.country)}>
+          {" "}
+          Add Country
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Country;
